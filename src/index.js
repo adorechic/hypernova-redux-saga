bb9284e5d657114b21080ab1e55bb8ef93bd6759
Takamichi Yoshikawa <t-yoshikawa@o-uccino.jp>
2018-04-16 17:12:15 +0900

    Add binding

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
+import hypernova, { serialize, load } from 'hypernova';
+
+export const renderReact = (name, component, store) => hypernova({
+  server() {
+    return (props) => {
+      const comp = React.createElement(component, props);
+      const promise = store.runSaga().done.then(() => {
+        const contents = ReactDOMServer.renderToString(comp);
+        return serialize(name, contents, props);
+      });
+
+      ReactDOMServer.renderToString(comp);
+      store.close();
+
+      return promise;
+    };
+  },
+
+  client() {
+    const payloads = load(name);
+
+    if (payloads) {
+      payloads.forEach((payload) => {
+        const { node, data } = payload;
+        const element = React.createElement(component, data);
+
+        if (ReactDOM.hydrate) {
+          ReactDOM.hydrate(element, node);
+        } else {
+          ReactDOM.render(element, node);
+        }
+      });
+    }
+
+    return component;
+  },
+});
