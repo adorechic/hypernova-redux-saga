9bb328bd6b878a3661724a8e143a6bf5a3a0710c
Takamichi Yoshikawa <t-yoshikawa@o-uccino.jp>
2018-04-19 11:41:05 +0900

    Fix interface

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import hypernova, { serialize, load } from 'hypernova';
 
 export const renderReact = (name, component, store) => hypernova({
   server() {
-    const promise = store.runSaga().done.then(() => {
-      const contents = ReactDOMServer.renderToString(component);
-      return serialize(name, contents, props);
-    });
+    return (props) => {
+      const element = React.createElement(component, props);
+      const promise = store.runSaga().done.then(() => {
+        const contents = ReactDOMServer.renderToString(element);
+        const initialStateLoader = `<script type="text/javascript" charset="utf-8">window.__INITIAL_STATE__ = ${JSON.stringify(store.getState())};</script>`
+        return serialize(name, contents + initialStateLoader, props);
+      });
 
-    ReactDOMServer.renderToString(component);
-    store.close();
+      ReactDOMServer.renderToString(element);
+      store.close();
 
-    return promise;
+      return promise;
+    };
   },
 
   client() {
